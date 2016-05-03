588fe3db02f873990dcb8404e61c94cbb78f5443
Stéphane Sire <s.sire@oppidoc.fr>
2016-05-03 22:37:31 +0200

    Extended regexp binding with new attributes to set a class on bound label when bound tree is valid, this can be used to change the color of the label when valid data is entered for instance; new attributes to also turn off validation on saving and to subscribe to axel-content-ready event to validate upon XML document loading

--- src/bindings/regexp.js
+++ src/bindings/regexp.js
@@ -17,20 +17,52 @@
   var _Regexp = {
 
     onInstall : function ( host ) {
-      var pattern = host.attr('data-pattern');
+      var pattern = host.attr('data-pattern'),
+          target = host.attr('data-target'),
+          root, jroot;
       this.re = new RegExp(this.getParam('regexp') || '');
       this.editor = $axel(host);
+      this.spec = host;
       host.bind('axel-update', $.proxy(this.checkRegexp, this));
       if (pattern) {
         host.find('input').attr("pattern", pattern); // adds HTML5 pattern attribute on input
       }
-      $axel.binding.setValidation(this.editor.get(0), $.proxy(this.checkRegexp, this));
+      if (this.spec.attr('data-validation') !== 'off') {
+        $axel.binding.setValidation(this.editor.get(0), $.proxy(this.checkRegexp, this));
+      }
+      if (target === 'document') {
+        jroot = $(document);
+      } else {
+        root = $axel.command.getEditor(target);
+        if (root) {
+          jroot = root.spec; // FIXME: $(root.getHost())
+        }
+      }
+      if (jroot) {
+        jroot.bind('axel-content-ready', $.proxy(this.checkRegexp, this));
+      }
     },
 
     methods : {
 
       checkRegexp : function  () {
-        var valid = this.re.test(this.editor.text());
+        var valid = this.re.test(this.editor.text()),
+            scope, label, doc = this.getDocument(),
+            anchor = this.spec.get(0),
+            klass = this.spec.attr('data-valid-class');
+        if (klass) {
+          if (! this.errScope) { // search error in full document
+            label = $('body ' + this.spec.attr('data-label-selector'), doc);
+          } else { // search error within scope
+            scope = $(anchor, doc).closest(this.errScope);
+            label = $(this.spec.attr('data-label-selector'), scope.get(0));
+          }
+          if (valid) {
+            label.addClass(klass);
+          } else {
+            label.removeClass(klass);
+          }
+        }
         return this.toggleError(valid, this.editor.get(0).getHandle(true));
       }
     }
